e0c38440bc507fd6a3b7928f4a5f69fd76c6e26e
Akanksha Choudhary <akanksha.ch29@gmail.com>
2024-09-30 20:20:53 +0530

    fix: auth issue

--- index.js
+++ index.js
@@ -14,10 +14,12 @@ const express = require(`express`);
 const app = express();
 const cors = require(`cors`);
 const corsOption = {
-  origin: [
-    "http://localhost:5173",
-    "https://project-1-frontend-six.vercel.app",
-  ],
+  origin: true,
+  // origin: [
+  //   "http://localhost:5173",
+  //   "https://project-1-frontend-six.vercel.app",
+  //   "*",
+  // ],
   allowedMethods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
