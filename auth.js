406566f31abd69944ff03e598688e6dae413bf4a
Akanksha Choudhary <akanksha.ch29@gmail.com>
2024-09-30 19:37:47 +0530

    auth issue

--- auth.js
+++ auth.js
@@ -1,10 +1,12 @@
 const JWT = require(`jsonwebtoken`);
 
+const token = JWT.sign({ user: "signed" }, process.env.JWT_SECRET);
+
 const auth = (req, res, next) => {
-  console.log("request to access", req.headers);
-  const { authorization } = req.headers;
-  console.log("auth", authorization);
-  JWT.verify(authorization, process.env.JWT_SECRET, (err, result) => {
+  console.log("request to access", token);
+  // const { authorization } = req.headers;
+  // console.log("auth", authorization);
+  JWT.verify(token, process.env.JWT_SECRET, (err, result) => {
     if (err) {
       res.status(404).json({ message: "UN-Authorized Access" });
     } else {
